219b7aa83be525377080663e6aa0405fee024aad
anuragpkul <anurag.p.kul@gmail.com>
2018-12-03 08:56:24 -0500

    Initial project add

---
+++ src/main/java/com/spring5/akpetclinic/AkPetClinicApplication.java
@@ -0,0 +1,12 @@
+package com.spring5.akpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AkPetClinicApplication.class, args);
+    }
+}
---
+++ src/test/java/com/spring5/akpetclinic/AkPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package com.spring5.akpetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AkPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
